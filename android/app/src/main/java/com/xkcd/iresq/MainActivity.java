96023550eae2a526f405f5bc6002a2b5295e4d3a
Aman Priyadarshi <aman.eureka@gmail.com>
2016-04-09 23:14:25 +0530

    Added beacon

--- android/app/src/main/java/com/xkcd/iresq/MainActivity.java
+++ android/app/src/main/java/com/xkcd/iresq/MainActivity.java
@@ -1,13 +1,70 @@
 package com.xkcd.iresq;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region(
+                        "monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                        36966, 3146));
+            }
+        });
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            //some random crappy shit!
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list)
+            {
+                showNotification("You are connected!");
+            }
+            @Override
+            public void onExitedRegion(Region region)
+            {
+                showNotification("You are disconnected!");
+            }
+        });
+    }
+    public void showNotification(String title) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.lifesaver)
+                .setContentTitle(title)
+                .setContentText("")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
