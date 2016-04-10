30e0b7ae3b8885cb212e39cf7217a198ad566bee
Laksh <laksharora28@gmail.com>
2016-04-10 14:10:09 +0530

    trained the model

--- server/test.py
+++ server/test.py
@@ -1,4 +1,17 @@
+# preparing dataset
+
 import random
 
 for i in range(10000):
-    print random.uniform(25,28),',',random.uniform(27,31),',', random.uniform(0,1)
+	min_t = random.uniform(25,30)
+	max_t = min_t + random.uniform(1,8)
+	mot = random.uniform(0,1)
+
+	human = 0
+	nHuman = 0
+	if min_t >= 25 and max_t <= 30 and mot >=0.5:
+		human = 1
+	else :
+		nHuman = 1
+
+	print min_t, max_t, mot, human, nHuman
--- server/utils/train.py
+++ server/utils/train.py
@@ -14,7 +14,7 @@ from math import sqrt
 import pylab
 import random
 import os.path
-from pybrain.structure import TanhLayer
+from pybrain.structure import SigmoidLayer
 from pybrain.datasets.supervised import SupervisedDataSet as SDS
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
@@ -22,53 +22,56 @@ from pybrain.tools.customxml.networkwriter import NetworkWriter
 from pybrain.tools.customxml.networkreader import NetworkReader
 
 
-class trainNeural():
+train_file = 'data/train.csv'
+# validation_file = 'data/validation.csv'
+output_model_file = 'model.pkl'
 
-    def __init__(self):
-        self.train_file = 'data/train.csv'
-        # self.validation_file = 'data/validation.csv'
-        self.output_model_file = 'model.pkl'
+# hidden_size = 4
+epochs = 500
 
-        self.hidden_size = 4
-        self.epochs = 50
+# load data
+# def loadData():
+train = np.loadtxt(train_file, delimiter=' ')
+Input = train[0:,0:3]
+Output = train[0:,3:5]
 
-    # load data
-    def loadData():
-        train = np.loadtxt(train_file, delimiter=',')
-        # validation = np.loadtxt(validation_file, delimiter=',')
-        # train = np.vstack((train, validation))
+# validation = np.loadtxt(validation_file, delimiter=',')
+# train = np.vstack((train, validation))
 
-        x_train = train[:, 0:-1]
-        y_train = train[:, -1]
-        y_train = y_train.reshape(-1, 1)
+# x_train = train[:, 0:-1]
+# y_train = train[:, -1]
+# y_train = y_train.reshape(-1, 1)
 
-        input_size = x_train.shape[1]
-        target_size = y_train.shape[1]
+# input_size = x_train.shape[1]
+# target_size = y_train.shape[1]
 
-    # prepare dataset
-    def prepare dataset():
-        ds = SDS(input_size, target_size)
-        ds.setField('input', x_train)
-        ds.setField('target', y_train)
+# prepare dataset
+# def prepare dataset(input_size, target_size):
+ds = SDS(Input,Output)
+# ds.addSample(input_size)
+# ds.setField('input', x_train)
+# ds.setField('target', y_train)
 
-    # init and train
-    def initTrain(input_size, hidden_size, ):
-        # net = buildNetwork(input_size, hidden_size, target_size, bias=True)
-        net = buildNetwork(3,  # input layer
-                                     4,  # hidden0
-                                     2,  # output
-                                     hiddenclass=TanhLayer,
-                                     outclass=TanhLayer,
-                                     bias=True
-                                     )
+# init and train
+# def initTrain(input_size, hidden_size, input, output):
+# net = buildNetwork(input_size, hidden_size, target_size, bias=True)
+net = buildNetwork(3,  # input layer
+                             4,  # hidden0
+                             2,  # output
+                             hiddenclass=SigmoidLayer,
+                             outclass=SigmoidLayer,
+                             bias=True
+                             )
+for i,o in zip(Input,Output):
+    ds.addSample(i,o)
 
-        trainer = BackpropTrainer(net, ds)
+trainer = BackpropTrainer(net, ds)
+    
+print "training for {} epochs...".format(epochs)
 
-        print "training for {} epochs...".format(epochs)
+for i in range(epochs):
+    mse = trainer.train()
+    rmse = sqrt(mse)
+    print "training RMSE, epoch {}: {}".format(i + 1, rmse)
 
-        for i in range(epochs):
-            mse = trainer.train()
-            rmse = sqrt(mse)
-            print "training RMSE, epoch {}: {}".format(i + 1, rmse)
-
-        pickle.dump(net, open(output_model_file, 'wb'))
+pickle.dump(net, open(output_model_file, 'wb'))
