97651e7f86d4a1a6ae365029d493552ae919162c
Manoj Pandey <manojpandey1996@gmail.com>
2016-04-10 13:37:23 +0530

    template edits

--- server/app.py
+++ server/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
 	return render_template('index.html')
 
+@app.route('/hacker')
+def hacker():
+	return render_template('hacker.html')
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
