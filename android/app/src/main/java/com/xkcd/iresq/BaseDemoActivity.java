c18646cf3cadd0b37cb2fe1bd8ae4311d06bb4c9
Aman Priyadarshi <aman.eureka@gmail.com>
2016-04-10 11:58:26 +0530

    Added to base activity

--- android/app/src/main/java/com/xkcd/iresq/BaseDemoActivity.java
+++ android/app/src/main/java/com/xkcd/iresq/BaseDemoActivity.java
@@ -2,18 +2,29 @@ package com.xkcd.iresq;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+import com.estimote.sdk.Utils;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by laksh on 10/4/16.
  */
 public abstract class BaseDemoActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-
+    protected GoogleMap mMap;
+    protected Region region;
+    protected BeaconManager beaconManager;
+    protected  List<Beacon> mBeacons;
     protected int getLayoutId() {
         return R.layout.map;
     }
@@ -23,12 +34,49 @@ public abstract class BaseDemoActivity extends FragmentActivity implements OnMap
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         setUpMap();
+
+        beaconManager = new BeaconManager(this);
+        region = new Region("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, final List<Beacon> rangedBeacons) {
+                // Note that results are not delivered on UI thread.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Just in case if there are multiple beacons with the same uuid, major, minor.
+                        for (Beacon rangedBeacon : rangedBeacons) {
+                            Log.d("[beacon]", "Rssi" + rangedBeacon.getRssi());
+                            Log.d("[beacon]", "Mac" + rangedBeacon.getMacAddress());
+                            Log.d("[beacon]", "Distance" + Math.min(Utils.computeAccuracy(rangedBeacon), 6.0));
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMap();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
     }
 
     @Override
